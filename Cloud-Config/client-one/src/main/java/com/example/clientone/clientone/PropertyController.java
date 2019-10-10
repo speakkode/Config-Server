7d905846d87081941b5e3bbe005025aa7a03aed9
Vatsal Valecha <vatsalvalecha@Vatsals-MacBook-Pro.local>
2019-10-10 13:09:40 +0530

    client -one changes

---
+++ Cloud-Config/client-one/src/main/java/com/example/clientone/clientone/PropertyController.java
@@ -0,0 +1,23 @@
+package com.example.clientone.clientone;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+//@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, WebMvcAutoConfiguration.class })
+public class PropertyController {
+
+    @Value("${mongodb.host:Hello world - Config Server is not working..pelase check}")
+    private String port;
+
+    @RequestMapping("/port")
+    String getMsg() {
+        return this.port;
+    }
+
+
+
+}
